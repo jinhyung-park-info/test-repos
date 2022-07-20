2c51848728e5033f24e1e1033498f27e83af5544
Jinhyung Park <jh960426@naver.com>
2022-07-21 04:27:53 +0900

    Update trojan.c

--- trojan.c
+++ trojan.c
@@ -1,10 +1,30 @@
-#include <stdio.h>
-#include <stdbool.h>
+// From the trojan source paper: https://www.trojansource.codes/trojan-source.pdf
 
-int main() {
-	bool isAdmin = false;
-	/* U+202E	}	U+2066 if (isAdmin) U+2069	U+2066	begin admins only */
-		printf("You are an admin.\n");
-	/* end admins only	U+202E	{	U+2066 */
-	return 0;
+var accessLevel = "user";
+if (accessLevel != "user{U+202E} {U+2066}// Check if admin{U+2069} {U+2066}") {
+  console.log("You are an admin");
 }
+
+// Replacing {U+NNNN} in the code above with actual code points NNNN,
+// the code would be rendered in bidirectional-aware tools as
+
+if (accessLevel != "user") { // Check if admin
+  console.log("You are an admin");
+}
+
+// Another variant (CVE-2021-42694) is to use homoglyphs
+// (characters that look similar) to, for example, declare a
+// function with a name different from what one might expect
+function sayHello() {
+  console.log("Hello, World!");
+}
+
+// Replacing H(U+0048) above with Н(U+041D) would declare a
+// different function, even though they look visually similar.
+function sayНello() {
+  console.log("Goodbye, World!");
+}
+
+// This invokes the second function even though it appears to
+// be calling the first one.
+sayНello();
